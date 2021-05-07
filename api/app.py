b20dc512af5d6d21191376be0834a96b8756fd13
Noah <n_kess@outlook.de>
2021-05-07 11:27:41 +0200

    first api

--- api/app.py
+++ api/app.py
@@ -1,1 +1,18 @@
-print("Hello World")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("landingpage.html")
+
+@app.route("/calculate", methods=["POST", "GET"])
+def calculate():
+
+    return render_template("result.html")
+
+
+
+if __name__ == '__main__':
+    app.run(port=1337, debug=True)
